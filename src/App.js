4a883657cb81b74b013e4dadb7794c2d7e7a5b88
ArnabShikder24 <arnabshikder2001@gmail.com>
2022-05-03 02:25:51 +0600

    home page finaly ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout></Layout>}></Route>
+        <Route path='/' element={<Layout></Layout>}>
+          <Route index element={<Home></Home>}></Route>
+        </Route>
       </Routes>
     </>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+import Logo from '../Logo/Logo';
+import './Home.scss'
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animation');
+    const nameArray = ['r', 'n', 'a', 'b'];
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'p', 'e', 'r', '.'];
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
+    return (
+        <div className='container home-page'>
+            <div className='text-zone'>
+                <h1>
+                    <span className={letterClass}>H</span> 
+                    <span className={`${letterClass} _15`}>i,</span> 
+                <br />
+                <span className={`${letterClass} _16`}>I</span> 
+                <span className={`${letterClass} _17`}>'m</span> 
+                <span className='name-letter'>A</span>
+                 <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={18}></AnimatedLetters>
+                <br />
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22}></AnimatedLetters>
+                </h1>
+                <h2>MERN Stack Developer / JavaScript Expert</h2>
+                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+            </div>
+            <Logo></Logo>
+        </div>
+    );
+};
+
+export default Home;
