a14df805f99cbd1ae84d58b08c1e2c27754b3a21
ArnabShikder24 <arnabshikder2001@gmail.com>
2022-12-18 23:18:31 +0600

    bugs fixed

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -8,7 +8,6 @@ import './Portfolio.scss';
 const Portfolio = () => {
     const [letterClass, setLetterClass] = useState('text-animation');
     const [portfolio, setPortfolio] = useState([]);
-    const [reverse, setReverse] = useState([]);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -24,10 +23,6 @@ const Portfolio = () => {
         getPortfolio();
     }, [])
 
-    useEffect(() => {
-        setReverse(portfolio.reverse());
-    },[portfolio])
-
 
     const getPortfolio = async () => {
         const querySnapshort = await getDocs(collection(db, 'portfolio'));
@@ -39,7 +34,7 @@ const Portfolio = () => {
         return (
             <div className='images-container'>
                 {
-                    portfolio.map((port, idx) => {
+                    portfolio.reverse().map((port, idx) => {
                         return (
                             <div className='image-box' key={idx}>
                                 <img className='portfolio-image' src={port.image} alt={port.title} />
@@ -66,7 +61,7 @@ const Portfolio = () => {
                     ></AnimatedLetters>
                 </h1>
 
-                <div>{renderPortfolio(reverse)}</div>
+                <div>{renderPortfolio(portfolio)}</div>
             </div>
             <Loader type='pacman'></Loader>
         </>
